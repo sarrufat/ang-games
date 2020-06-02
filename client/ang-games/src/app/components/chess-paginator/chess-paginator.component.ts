0fd08f99087e15acfe16cbb8d3eea539a06f3c3c
sarrufat <arrufat.sergi@gmail.com>
2020-06-02 09:39:45 +0200

    Paginator adjustments

--- client/ang-games/src/app/components/chess-paginator/chess-paginator.component.ts
+++ client/ang-games/src/app/components/chess-paginator/chess-paginator.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { ChessResult, Combination } from '../../service/chess-result-model'
 
 
@@ -8,23 +8,30 @@ import { ChessResult, Combination } from '../../service/chess-result-model'
   templateUrl: './chess-paginator.component.html',
   styleUrls: ['./chess-paginator.component.scss']
 })
-export class ChessPaginatorComponent implements OnInit {
+export class ChessPaginatorComponent implements OnInit, OnChanges {
 
   @Input()
   chessResult: ChessResult;
   @Input()
   dimension: number;
-
+  pageSize = 4;
 
   dataSource: Combination[];
 
   constructor() { }
+  ngOnChanges(changes: SimpleChanges): void {
+    if (this.chessResult !== undefined) {
+      this.pageSize = this.dimension <= 5 ? 4 : 2;
+    }
+  }
+
 
   ngOnInit(): void {
-    this.dataSource = this.chessResult.combinations.slice(0, 4);
+    this.dataSource = this.chessResult.combinations.slice(0, this.pageSize);
   }
 
   fetch(page: number): void {
-    this.dataSource = this.chessResult.combinations.slice(page * 4, page * 4 + 4);
+    const pg = this.pageSize;
+    this.dataSource = this.chessResult.combinations.slice(page * pg, page * pg + pg);
   }
 }
